4bea701c2d53b0a516f2547f9614c73187f5be2e
LongVu1611 <118415481+LongVu1611@users.noreply.github.com>
2023-11-09 22:00:06 +0700

    Add project files.

---
+++ demo/View/ChildForm/frmPhieuthue.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace demo.View.ChildForm
+{
+    partial class frmPhieuthue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(160, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(160, 119);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(190, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(160, 197);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(190, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(643, 54);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(643, 119);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker2.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 251);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(931, 180);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Số phiếu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Mã khách hàng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Mã nhân viên";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(521, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Ngày cho thuê";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(556, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Ngày trả";
+            // 
+            // frmPhieuthue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1061, 443);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "frmPhieuthue";
+            this.Text = "frmPhieuthue";
+            this.Load += new System.EventHandler(this.frmPhieuthue_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ demo/View/ChildForm/frmPhieuthue.cs
@@ -0,0 +1,105 @@
+﻿using demo.Controller;
+using demo.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace demo.View.ChildForm
+{
+    public partial class frmPhieuthue : Form
+    {
+        KhachController khachController;
+        SanphamController sanphamController;
+        PhieuthueController phieuthueController;
+        ChitietController chitietController;
+        List<Sanpham> ds_sanpham;//danh sach kho
+        List<Khach> ds_khach;//danh sach hang hoa
+        Phieuthue currentphieu;
+        public frmPhieuthue()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 0)
+            {
+                String idsp = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString().Trim();
+                dataGridView1.Rows[e.RowIndex].Cells[1].Value = sanphamController.get(idsp).getNamesp();
+                dataGridView1.Rows[e.RowIndex].Cells[2].Value = sanphamController.get(idsp).getGiavon();
+
+            }
+            //column index 4 là đơn giá
+            if (e.ColumnIndex == 4)
+            {
+                int sn = int.Parse(dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString().Trim());
+                int hd = int.Parse(dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString().Trim());
+                dataGridView1.Rows[e.RowIndex].Cells[5].Value = sn * hd;
+
+            }
+        }
+
+        private void frmPhieuthue_Load(object sender, EventArgs e)
+        {
+            ds_sanpham = new List<Sanpham>(); //chứa danh sách kho
+            sanphamController = new SanphamController();//Kho controller
+            //ds_khach = khachController.load();
+            //ds_khach = new List<Khach>();
+            chitietController = new ChitietController();
+            sanphamController = new SanphamController();
+            ds_sanpham = sanphamController.load();
+            phieuthueController = new PhieuthueController();
+
+            foreach (Sanpham k in ds_sanpham)
+            {
+                comboBox1.Items.Add(k.getIdsp());
+
+            }
+            DataGridViewComboBoxColumn comboboxColumn;
+            comboboxColumn = new DataGridViewComboBoxColumn();
+            comboboxColumn.DataPropertyName = "masp";
+            comboboxColumn.HeaderText = "Mã sản phẩm";
+            comboboxColumn.DropDownWidth = 160;
+            comboboxColumn.Width = 90;
+            comboboxColumn.MaxDropDownItems = 3;
+            comboboxColumn.FlatStyle = FlatStyle.Flat;
+            foreach (Sanpham sp in ds_sanpham)
+            {
+                comboboxColumn.Items.Add(sp.getIdsp());
+            }
+            dataGridView1.Columns.Clear();
+            dataGridView1.Columns.Add(comboboxColumn);//Ma san pham
+            //tên hàng
+            DataGridViewTextBoxColumn colTenHang = new DataGridViewTextBoxColumn();
+            colTenHang.DataPropertyName = "tensp";
+            colTenHang.HeaderText = "Tên sản phẩm";
+            dataGridView1.Columns.Add(colTenHang);
+            // Đơn vị tính
+            DataGridViewTextBoxColumn colGiavon = new DataGridViewTextBoxColumn();
+            colGiavon.DataPropertyName = "giavon";
+            colGiavon.HeaderText = "Giá vốn";
+            dataGridView1.Columns.Add(colGiavon);
+            // Số ngày thuê
+            DataGridViewTextBoxColumn colSoLuong = new DataGridViewTextBoxColumn();
+            colSoLuong.DataPropertyName = "giathue1ngay";
+            colSoLuong.HeaderText = "Giá thuê 1 ngày";
+            dataGridView1.Columns.Add(colSoLuong);
+            // Đơn giá
+            DataGridViewTextBoxColumn colDongia = new DataGridViewTextBoxColumn();
+            colDongia.DataPropertyName = "giathue1ngay";
+            colDongia.HeaderText = "Đơn giá";
+            dataGridView1.Columns.Add(colDongia);
+            // Thành tiền
+            DataGridViewTextBoxColumn colThanhTien = new DataGridViewTextBoxColumn();
+            colThanhTien.DataPropertyName = "ThanhTien";
+            colThanhTien.HeaderText = "Thành tiền";
+            dataGridView1.Columns.Add(colThanhTien);
+        }
+    }
+}
